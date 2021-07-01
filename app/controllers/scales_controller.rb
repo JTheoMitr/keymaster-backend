c2159febc4292bd46cb1740b0f6a10f69a112ddf
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-07-01 16:12:04 -0500

    added create and scale params to scales controller

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -5,4 +5,21 @@ class ScalesController < ApplicationController
         render json: scales.to_json(except: [:created_at, :updated_at])
     end
 
+    def create
+        scale = Scale.new(scale_params)
+        if scale.save
+            render json: scale.to_json(except: [:created_at, :updated_at])
+        else
+            render json: {error: "Could not save new scale"}
+        end
+
+    end
+
+
+    private
+
+    def scale_params
+        params.require(:scale).permit(:name, :notes, :description)
+    end
+
 end
